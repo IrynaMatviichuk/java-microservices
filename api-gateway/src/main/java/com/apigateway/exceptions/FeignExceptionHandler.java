0b5fae3889b3551b423037aa65df1253acebc63a
Iryna Matviichuk <ira.mia.m99@gmail.com>
2020-04-26 16:58:44 +0300

    add grafana, prometheus

--- api-gateway/src/main/java/com/apigateway/exceptions/FeignExceptionHandler.java
+++ api-gateway/src/main/java/com/apigateway/exceptions/FeignExceptionHandler.java
@@ -3,17 +3,20 @@ package com.apigateway.exceptions;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class FeignExceptionHandler {
 
     @ExceptionHandler(FeignException.BadRequest.class)
-    public ResponseEntity handleFeignStatusException(FeignException ex) throws JsonProcessingException {
-        return ResponseEntity.status(ex.status()).body(new ObjectMapper().readValue(ex.contentUTF8(), HashMap.class));
+    public Map<String, Object> handleFeignStatusException(FeignException ex) throws JSONException {
+        return new JSONObject(ex.contentUTF8()).toMap();
     }
 }
